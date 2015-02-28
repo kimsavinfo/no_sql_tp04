0283e0245bac0178b86d4dfcb4f9e56b354610a9
kimsavinfo <kimsavinfo@gmail.com>
2015-02-28 18:09:49 +0100

    AQL queries done for family links

---
+++ main_arrangoDB.js
@@ -0,0 +1,165 @@
+//  > arangosh
+
+
+
+// =================================
+//  AQL Queries
+// =================================
+
+//  Find parents
+FOR kid IN gameOfThrone 
+	FILTER kid.name == "Sansa Stark"
+	FOR link IN family_edge 
+		FILTER link._to == CONCAT("gameOfThrone/", kid._key)
+		FOR parent IN gameOfThrone
+			FILTER parent._key == SUBSTRING(link._from, (FIND_LAST(link._from, "/") + 1) )
+		RETURN
+		{
+			"parent name" : parent.name,
+			"parent key" : parent._key,
+			"parent gender" : parent.gender
+		}
+
+//  Find the father
+FOR kid IN gameOfThrone 
+	FILTER kid.name == "Sansa Stark"
+	FOR link IN family_edge 
+		FILTER link._to == CONCAT("gameOfThrone/", kid._key)
+		FOR father IN gameOfThrone
+			FILTER father._key == SUBSTRING(link._from, (FIND_LAST(link._from, "/") + 1) )
+			AND father.gender == "m"
+		RETURN
+		{
+			"parent name" : father.name,
+			"parent key" : father._key
+		}
+
+//  Find the mother
+FOR kid IN gameOfThrone 
+	FILTER kid.name == "Sansa Stark"
+	FOR link IN family_edge 
+		FILTER link._to == CONCAT("gameOfThrone/", kid._key)
+		FOR father IN gameOfThrone
+			FILTER father._key == SUBSTRING(link._from, (FIND_LAST(link._from, "/") + 1) )
+			AND father.gender == "f"
+		RETURN
+		{
+			"parent name" : father.name,
+			"parent key" : father._key
+		}
+
+//  Find the kids
+FOR parent IN gameOfThrone
+	FILTER parent.name == "Eddard Stark"
+	AND parent.gender == "m"
+	FOR link IN family_edge 
+		FILTER link._from == CONCAT("gameOfThrone/", parent._key)
+		FOR kid IN gameOfThrone
+			FILTER kid._key == SUBSTRING(link._to, (FIND_LAST(link._to, "/") + 1) )
+		RETURN
+		{
+			"kid name" : kid.name,
+			"kid key" : kid._key,
+			"kid gender" : kid.gender
+		}
+
+//  Find the daughters
+FOR parent IN gameOfThrone
+	FILTER parent.name == "Eddard Stark"
+	AND parent.gender == "m"
+	FOR link IN family_edge 
+		FILTER link._from == CONCAT("gameOfThrone/", parent._key)
+		FOR kid IN gameOfThrone
+			FILTER kid._key == SUBSTRING(link._to, (FIND_LAST(link._to, "/") + 1) )
+			AND kid.gender == "f"
+		RETURN
+		{
+			"kid name" : kid.name,
+			"kid key" : kid._key,
+			"kid gender" : kid.gender
+		}
+
+//  Find the sons
+FOR parent IN gameOfThrone
+	FILTER parent.name == "Eddard Stark"
+	AND parent.gender == "m"
+	FOR link IN family_edge 
+		FILTER link._from == CONCAT("gameOfThrone/", parent._key)
+		FOR kid IN gameOfThrone
+			FILTER kid._key == SUBSTRING(link._to, (FIND_LAST(link._to, "/") + 1) )
+			AND kid.gender == "m"
+		RETURN
+		{
+			"kid name" : kid.name,
+			"kid key" : kid._key,
+			"kid gender" : kid.gender
+		}
+
+//  Find the brothers and sisters
+FOR kid IN gameOfThrone 
+	FILTER kid.name == "Sansa Stark"
+	FOR parentLink IN family_edge 
+		FILTER parentLink._to == CONCAT("gameOfThrone/", kid._key)
+		FOR siblingsLink IN family_edge
+			FILTER siblingsLink._from == parentLink._from
+			FOR siblings IN gameOfThrone
+				FILTER siblings._key == SUBSTRING(siblingsLink._to, (FIND_LAST(siblingsLink._to, "/") + 1) )
+				AND siblings._key != kid._key
+			RETURN
+			{
+				"sibling name" : siblings.name,
+				"sibling key" : siblings._key,
+				"sibling gender" : siblings.gender
+			}
+
+//  Find sisters
+FOR kid IN gameOfThrone 
+	FILTER kid.name == "Sansa Stark"
+	FOR parentLink IN family_edge 
+		FILTER parentLink._to == CONCAT("gameOfThrone/", kid._key)
+		FOR siblingsLink IN family_edge
+			FILTER siblingsLink._from == parentLink._from
+			FOR siblings IN gameOfThrone
+				FILTER siblings._key == SUBSTRING(siblingsLink._to, (FIND_LAST(siblingsLink._to, "/") + 1) )
+				AND siblings.gender == "f"
+				AND siblings._key != kid._key
+			RETURN
+			{
+				"sibling name" : siblings.name,
+				"sibling key" : siblings._key,
+				"sibling gender" : siblings.gender
+			}
+
+//  Find brothers
+FOR kid IN gameOfThrone 
+	FILTER kid.name == "Sansa Stark"
+	FOR parentLink IN family_edge 
+		FILTER parentLink._to == CONCAT("gameOfThrone/", kid._key)
+		FOR siblingsLink IN family_edge
+			FILTER siblingsLink._from == parentLink._from
+			FOR siblings IN gameOfThrone
+				FILTER siblings._key == SUBSTRING(siblingsLink._to, (FIND_LAST(siblingsLink._to, "/") + 1) )
+				AND siblings.gender == "m"
+				AND siblings._key != kid._key
+			RETURN
+			{
+				"sibling name" : siblings.name,
+				"sibling key" : siblings._key,
+				"sibling gender" : siblings.gender
+			}
+
+//  Find grand-parents
+FOR kid IN gameOfThrone 
+	FILTER kid.name == "Sansa Stark"
+	FOR parentLink IN family_edge 
+		FILTER parentLink._to == CONCAT("gameOfThrone/", kid._key)
+		FOR grandParentLink IN family_edge
+			FILTER grandParentLink._to == parentLink._from
+			FOR grandParent IN gameOfThrone
+				FILTER grandParent._key == SUBSTRING(grandParentLink._to, (FIND_LAST(grandParentLink._to, "/") + 1) )
+			RETURN
+			{
+				"grand-parent name" : grandParent.name,
+				"grand-parent key" : grandParent._key,
+				"grand-parent gender" : grandParent.gender
+			}
